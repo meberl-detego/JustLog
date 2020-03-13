96ef3e4c473e0a354f890db2c09985d6f677a21d
teodorhtec <45098998+teodorhtec@users.noreply.github.com>
2020-03-13 11:58:22 +0100

    Quick fix - userInfo casted to [String: Any]

--- JustLog/Extensions/Dictionary+Flattening.swift
+++ JustLog/Extensions/Dictionary+Flattening.swift
@@ -37,8 +37,8 @@ extension Dictionary where Key == String {
             case is Array<Any>:
                 retVal.updateValue(String(describing: v), forKey: k)
             case is NSError:
-                if let inner = v as? NSError {
-                    retVal = retVal.merged(with: inner.userInfo.flattened())
+                if let userInfo = (v as? NSError)?.userInfo as? [String: Any] {
+                    retVal = retVal.merged(with: userInfo.flattened())
                 }
                 else {
                     continue
